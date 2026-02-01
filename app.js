fbcd9f8c151479b02927c7c86f6f7b578bc5273a
Seno <aseno001cladmin@msd.govt.nz>
2026-02-01 14:48:31 +1300

    html header

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const PORT = 7070;
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write(`
     <html>
       <head>
